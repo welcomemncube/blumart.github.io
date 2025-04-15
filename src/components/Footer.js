eb68d23f0acab515c0762cbbabc3d3b50998f878
Welcome Mncube <39636648+welcomemncube@users.noreply.github.com>
2025-04-15 13:44:26 +0200

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+const Footer = () => (
+    <footer className="bg-blue-800 text-white py-6">
+        <div className="container mx-auto text-center">
+            <p>© 2025 BluMart. All rights reserved.</p>
+            <div className="flex justify-center space-x-4 mt-4">
+                <a href="#" className="hover:underline">Facebook</a>
+                <a href="#" className="hover:underline">Instagram</a>
+                <a href="#" className="hover:underline">Twitter</a>
+            </div>
+        </div>
+    </footer>
+);
